0b177a6f383438014141815541ef04ecc3b64b8e
Meaghan De Klerk <meaghan.dk@gmail.com>
2019-06-14 00:07:13 +1200

    started doing basic layout

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -6,29 +6,21 @@ class Home extends React.Component {
         return(
             <Fragment>
 
-                <div className="main_content">
+                <div className="contentHolder">
 
                     <h1>But Who Are You Really Though?</h1>
-
-                    <div className ="info box">
 
                         <p>
                             My name is Meaghan De Klerk, and I have no idea what I am doing with my life, but what I do know is that I have passion
                             for coding and that I would love to do something with it as a career. I am originally from South Africa but now 
                             call Wellington, New Zealand home. I have many loves one of them being music and collecting vinyls. Another for gaming
                             and creating art. 
-                            <br/>
+                        </p>
+                        <p>
                             Travelling is also something I'm interested in but it is also the reason why I decided to pursue web development, 
                             as there are many places around the world looking for developers and therefore a tool in getting to see the world.
                             I want to experience everything the world has to offer or as much as I can during my time on this big rock hurling through space.
                         </p>
-
-                    </div>  
-
-
-                </div>
-
-                <div className="selfie">
 
 
                     <img src={portrait} alt="Picture of Meaghan De Klerk" id="meimg" />
